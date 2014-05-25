315acfaacb1cf3875cb3a72a98dfc50a12b3cbbf
Shawn Bierman <shawnbierman@me.com>
2014-05-25 08:13:55 -0500

    first commit

---
+++ getip.js
@@ -0,0 +1,35 @@
+// return list of 'active' interfaces
+
+var sys = require('sys'),
+	exec = require('child_process').exec,
+	child;
+
+child = exec("ifconfig", function (error, stdout, stderr) {
+
+	var lines = stdout.toString().split('\n'),
+		found = {},
+		interface = '';
+
+	lines.forEach( function (line) {
+
+		if( line[0] != '\t' && line[0] != undefined  && line[0] != null ) {
+
+			// found a line that begins with an interface name
+			interface = line.substring(0, line.indexOf(':'));
+	
+		} else {
+	
+			// find the IP of the found interface
+			var str = /inet /;
+	
+			if ( line.match(str) ) {
+				found[interface] = line.split(' ')[1];
+			}
+	
+		}
+
+	});
+
+	console.dir(found);
+
+});
